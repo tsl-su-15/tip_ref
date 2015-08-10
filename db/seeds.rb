c32ed3a67eabf4a8e8c1119d3c8e081959dbbceb
Arjun Venkataswamy <avenkat2@gmail.com>
2015-08-10 11:21:43 -0500

    begins seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Service.destroy_all
+services = [
+  { name: 'barista', avg_tip_percent: 0, avg_tip_flat_fee: 0 },
+  { name: 'bartender', avg_tip_percent: 15, avg_tip_flat_fee: 1 }
+]
+
+services.each do |service_hash|
+  Service.create(service_hash)
+end
+puts "#{Service.count} services are in the database"
