5c8325a413c49b9aae31d10a2236ab73f9bf5f92
Arjun Venkataswamy <avenkat2@gmail.com>
2015-08-11 12:05:01 -0500

    completes search results feature

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -7,6 +7,23 @@ class TipsController < ApplicationController
     @service = Service.find(params[:service_id])
     @num_items = params[:num_items].to_i
 
-    @suggested_tip = @num_items * @service.per_unit_tip
+    if @num_items.present? && @service.per_unit_tip > 0
+      @suggested_tip = @num_items * @service.per_unit_tip
+    elsif @service.avg_tip_percent > 0
+      @suggested_tip = (@service.avg_tip_percent/100.0) * params[:amount_spent].to_f
+      @total_bill = params[:amount_spent].to_f + @suggested_tip
+    elsif @service.per_unit_tip > 0
+      @suggested_tip = @num_items * @service.per_unit_tip
+    elsif @service.min_tip > 0
+      @suggested_tip = @service.min_tip
+    else
+      @suggested_tip = 0
+    end
+
+    if @service.min_tip > 0 && @suggested_tip < @service.min_tip
+      @suggested_tip = @min_tip
+    end
+
+
   end
 end
