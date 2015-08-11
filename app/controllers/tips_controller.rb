5dc8068fa85621cffe2c6997b1190396ebe82f95
Arjun Venkataswamy <avenkat2@gmail.com>
2015-08-11 11:07:43 -0500

    supports one type of service

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -4,6 +4,9 @@ class TipsController < ApplicationController
   end
 
   def search_results
+    @service = Service.find(params[:service_id])
+    @num_items = params[:num_items].to_i
 
+    @suggested_tip = @num_items * @service.per_unit_tip
   end
 end
